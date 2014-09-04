6883f616ed53138b969cfc5670c5f79b722eb0f9
airyland <airyland@qq.com>
2014-09-04 11:13:20 +0800

    use seedit-config

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var $ = require('jquery');
 require('iframe-ajax')($);
+var Config = require('seedit-config');
 
 module.exports = function(target, cb) {
     var $target = $(target),
         width = $target.width(),
         height = $target.height(),
-        domain = /seedit.com/.test(location.href) ? 'seedit.com' : 'online.bozhong.com';
+        domain = Config.getMainDomain().replace(/:\d+/, '');
     document.domain = domain;
     var $form = $('<form id="form" enctype="multipart/form-data" method="POST" style="opacity:0;"><input name="file" type="file" id="file" class="mk-file" style="display: block;width: ' + width + 'px;height: ' + height + 'px;"><input type="hidden" name="class" value="user"></form>');
     var $input = $form.find('.mk-file');
     $target.append($form);
     $input.change(function() {
-        console.log('change', $(this).val());
+
         $.ajax({
             url: 'http://image.' + domain + '/upload.php?__format=iframe',
             type: 'POST',
